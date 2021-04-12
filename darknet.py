2f294cfc2e5503a5d8ce9132f38a87b31c31da50
Carlos Augusto Hincapié R <solrac.hincapie@gmail.com>
2021-04-12 18:42:29 -0500

    Cambio de referencia a libdarknet.so

--- darknet.py
+++ darknet.py
@@ -220,11 +220,12 @@ if os.name == "nt":
             lib = CDLL(winGPUdll, RTLD_GLOBAL)
             print("Environment variables indicated a CPU run, but we didn't find {}. Trying a GPU run anyway.".format(winNoGPUdll))
 else:
-    print("Ruta en donde busca libdarknet.so:", os.path.join(
-        os.environ.get('DARKNET_PATH', './')))
-    lib = CDLL(os.path.join(
-        os.environ.get('DARKNET_PATH', './'),
-        "libdarknet.so"), RTLD_GLOBAL)
+    # print("Ruta en donde busca libdarknet.so:", os.path.join(
+    #     os.environ.get('DARKNET_PATH', './')))
+    # lib = CDLL(os.path.join(
+    #     os.environ.get('DARKNET_PATH', './'),
+    #     "libdarknet.so"), RTLD_GLOBAL)
+    lib = CDLL(os.path.join(os.getcwd(), "libdarknet.so"), RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
